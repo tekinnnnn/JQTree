3d8d888a17f767543bddca7828dbca2e27479190
Tekin Aydoğdu <tekinaydogdu01@gmail.com>
2015-04-18 13:55:54 +0300

    slideDown is OK

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Tekin
+ * User: Tekin Aydoğdu
  * Date: 18.4.2015
  * Time: 12:26
  */
 
 ?>
-
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title></title>
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$("ul li ul").attr("id", "hidden").hide();
+			$("li").click(function () {
+				$(this).children("#hidden").slideDown("fast").attr("id", "visible");
+			});
+		})
+	</script>
+</head>
+<body>
 <ul>
 	<li>asd</li>
 	<li>asd</li>
@@ -34,7 +49,9 @@
 			<li>asd</li>
 			<li>asd</li>
 			<li>asd</li>
-			<li>qwe</li>
+			<li>asd</li>
 		</ul>
 	</li>
 </ul>
+</body>
+</html>
